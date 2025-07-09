3c42d11961eea9db39906e391e5e35c0ca479b3b
Simon Pontaroli <simonpontaroli@gmail.com>
2025-07-09 00:51:21 -0300

    Configuro app para usar puerto dinámico en Render

--- app.py
+++ app.py
@@ -53,4 +53,6 @@ def obras():
     return render_template("works.html", works=works)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
